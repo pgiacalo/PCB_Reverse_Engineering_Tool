f8c7d5814830e0db6c79f7020e5ffdf08f63e3f1
Philip Giacalone <philip_giacalone@yahoo.com>
2025-12-09 20:05:37 -0800

    Remove redundant Author lines from license headers
    All files already include 'Copyright (c) 2025 Philip L. Giacalone' which provides
    sufficient attribution. The separate Author line is redundant and not part of
    the standard MIT License format.
    
    - Removed Author line from all code file headers
    - Removed Author line from LICENSE file
    - Removed Author section from README.md
    - Kept author field in package.json (standard npm field)

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 /**
  * Copyright (c) 2025 Philip L. Giacalone
- * 
- * Author: Philip L. Giacalone
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
